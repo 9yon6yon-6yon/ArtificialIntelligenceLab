b995aef3c7585a6b8e1ca0c4b44bf36435b167d9
Md. Asif Karim Chayon <mchayon201366@bscse.uiu.ac.bd>
2022-07-02 14:46:54 +0600

    Question 9 solved

--- Q9.py
+++ Q9.py
@@ -1,9 +1,19 @@
-employes = []
+class Employee:
+    def __init__(self, name, height, age):
+        self.name = name
+        self.height = height
+        self.age = age
 
-def takeInput():
-    name = input("Enter name : ")
-    height = int(input("Height in cm : "))
-    age = int(input("Enter age : "))
-n = int (input("How manys input : "))
-for i in range(n):
-    takeInput()
+employees = []
+N = int(input())
+for i in range(N):
+    name = input()
+    height = int(input())
+    age = int(input())
+    emp = Employee(name, height, age)
+    employees.append(emp)
+
+
+employees.sort(key=lambda x: (x.height, x.age), reverse=True)
+for i in employees:
+    print(emp.name, emp.height, emp.age)
